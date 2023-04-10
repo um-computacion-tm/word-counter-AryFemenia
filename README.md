2b09b79fa00c11c60c96ec60c6c1f6bd800941b2
AryFemenia <ad.femenia@alumno.um.edu.ar>
2023-04-09 21:34:56 -0300

    v0.3

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Contador
-### `Palabras`
-1) 
-2) 
-
 ### `Letras`
+Muestra la cantidad de letras que tiene cada palabra, incluyendo letras repetidas y espacios.
 
+### `Palabras`
+Muestra la cantidad de veces que se escribe una palabra.
--- count_test.py
+++ count_test.py
@@ -8,17 +8,22 @@ class TestCountLetters(unittest.TestCase):
     def test_simple(self):
         result = count_letters('a')
         self.assertEqual(result, {'a': 1})
+    def test_simple2(self):
+        result = count_letters('b')
+        self.assertEqual(result, {'b': 1})
+
     def test_complex(self):
-        result = count_letters('hola')
+        result = count_letters('aaabcd')
         self.assertEqual(
             result,
             {
-                'h': 1,
-                'o': 1,
-                'l': 1,
-                'a': 1,
+                'a': 3,
+                'b': 1,
+                'c': 1,
+                'd': 1,
             }
         )
+
     def test_super_complex(self):
         result = count_letters('hola chau')
         self.assertEqual(
@@ -37,8 +42,17 @@ class TestCountLetters(unittest.TestCase):
 #TEST WORD COUNT
 class TestCountWords(unittest.TestCase):
     def test_simple(self):
+        result = count_words('saracatunga')
+        self.assertEqual(result, {'saracatunga': 1})
+    def test_simple2(self):
         result = count_words('hola')
         self.assertEqual(result, {'hola': 1})
+    def test_simple3(self):
+        result = count_words('hola')
+        self.assertEqual(result, {'hola': 1})
+    def test_simple4(self):
+        result = count_words('hello')
+        self.assertEqual(result, {'hello': 1})
 
 if __name__ == '__main__':
     unittest.main()
